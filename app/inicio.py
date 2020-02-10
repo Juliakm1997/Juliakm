54d05f0f104a2266569e9428ca7beb8032a3c98f
Júlia Kanitz Marchi <67636@PROWAY.TREINA>
2020-02-10 20:31:23 -0300

    Aula 1 - PrintInput

---
+++ app/inicio.py
@@ -0,0 +1,23 @@
+# #-------- CTRL + K + C (comentar)
+# #-------- CTRL +K + U
+# #---- Imprimir info na tela
+# #---- Pular linha
+# print('\n')
+# # #---- Multiplicação de caractere
+# print('*'*50)
+# print('Olá, Usuário!')
+# print('Olá Júlia!')
+# print('\n')
+
+# #---- Pegar entrada do usuário
+# nome = input('Digite seu nome:')
+# sobrenome = input('Digite seu sobrenome:')
+
+# #---- Usando a função "format" para concatenação de string
+# print('Olá, {} {}'.format(nome, sobrenome))
+
+#---- Interpolação de strings
+print(f'Olá, {nome} {sobrenome}')
+
+idade = int (input('Digite a idade:'))
+print(idade)
